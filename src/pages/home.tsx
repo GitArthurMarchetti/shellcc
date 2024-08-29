785e6c3d4031488386a9ee4b3c65f6978ff29503
duduborges <duduborges333969@gmail.com>
2024-08-29 19:56:29 -0300

    atualizado

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,9 +10,9 @@ import { MdMailOutline } from "react-icons/md"
 export default function Home() {
     return (
         <>
-            <header>
+            <header className="w-full">
                 <nav className="flex flex-row w-full py-6  justify-between p-2 pl-5 pr-5 bg-gray-800">
-                    <div className="w-[20%] flex-row align-middle items-center ml-8 fonthome flex text-2xl justify-between text-primaryColor">
+                    <div className="w-[25%] flex-row  items-center ml-8 gap-3 fonthome flex text-2xl justify-between text-primaryColor">
                         <a href="">
                             Início
                         </a>
@@ -28,7 +28,7 @@ export default function Home() {
 
                     </div>
 
-                    <div className="w-[15%] flex flex-row mr-8 justify-between ">
+                    <div className="w-[15%] flex flex-row mr-8 gap-2 justify-between ">
                         <Button className="rounded-full text-xl p-6 border-white borderDashed ">
                             Criar conta
                         </Button>
@@ -66,9 +66,9 @@ export default function Home() {
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" className="z-10 relative" viewBox="0 0 1440 320"><path fill="#0e1125" fill-opacity="1" d="M0,128L60,144C120,160,240,192,360,181.3C480,171,600,117,720,96C840,75,960,85,1080,117.3C1200,149,1320,203,1380,229.3L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
                 </section>
-                <section className="bg-[#0e1125] flex flex-row justify-around text-2xl fonthome align-middle items-center">
+                <section className="bg-[#0e1125] flex flex-row justify-around text-2xl fonthome align-middle items-center" id="objetivo">
                     <div className="w-1/3">
-                        <p className="text-3xl font-bold underline" id="objetivo">
+                        <p className="text-3xl font-bold underline">
                             Objetivo
                         </p>
                         <p>
@@ -79,14 +79,15 @@ export default function Home() {
                         <img className="rounded-[50%] h-72 w-72 flutuando" src={tartaruga} alt="" />
                     </div>
                 </section>
-                <section><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0e1125" fill-opacity="1" d="M0,64L60,85.3C120,107,240,149,360,181.3C480,213,600,235,720,218.7C840,203,960,149,1080,128C1200,107,1320,117,1380,122.7L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+                <section >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0e1125" fill-opacity="1" d="M0,64L60,85.3C120,107,240,149,360,181.3C480,213,600,235,720,218.7C840,203,960,149,1080,128C1200,107,1320,117,1380,122.7L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
 
-                    <section className="flex flex-row justify-around text-2xl fonthome align-middle items-center">
+                    <section className="flex flex-row justify-around text-2xl fonthome align-middle items-center" id="equipe">
                         <div>
                             <img className="rounded-[50%] h-72 w-72 flutuando" src={tartarugaVermelha} alt="" />
                         </div>
                         <div className="w-1/3">
-                            <p className="text-3xl font-bold underline" id="equipe">
+                            <p className="text-3xl font-bold underline" >
                                 Equipe
                             </p>
                             <p>
@@ -98,10 +99,10 @@ export default function Home() {
                     </section>
                 </section>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0e1125" fill-opacity="1" d="M0,64L60,85.3C120,107,240,149,360,181.3C480,213,600,235,720,218.7C840,203,960,149,1080,128C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-                <section className="bg-[#0e1125] flex flex-row justify-around text-2xl fonthome align-middle items-center">
+                <section className="bg-[#0e1125] flex flex-row justify-around text-2xl fonthome align-middle items-center" id="sobre">
 
                     <div className="w-1/3">
-                        <p className="text-3xl font-bold underline" id="sobre">
+                        <p className="text-3xl font-bold underline" >
                             Sobre a Shellcc
                         </p>
                         <p>
@@ -120,7 +121,7 @@ export default function Home() {
 
             </main >
             <footer className="bg-black w-full h-full py-6 text-primaryColor">
-                <section className="flex flex-row w-11/12  m-auto justify-around">
+                <section className="flex flex-row w-11/12  m-auto justify-around" >
                     <div>
                         <div className="text-center mb-4">
                             <h2 className="fontTitle text-7xl">
