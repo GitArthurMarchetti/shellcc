720bed6000617bfab1aa917f0e5241b831b3cdd1
Luiza Fischer <127206345+LuizaFischer@users.noreply.github.com>
2024-08-29 22:05:13 -0300

    login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { GiTurtle } from "react-icons/gi"
+import { GiTurtle } from "react-icons/gi";
 import { IoEye, IoEyeOff } from "react-icons/io5";
 
 function Login() {
@@ -9,21 +9,21 @@ function Login() {
         <>
             <main className="bg-bgColor h-[100vh] text-primaryColor">
                 <section className="flex w-11/12 m-auto justify-center items-center h-full">
-                    <div className="flex border-dashed flex-col text-center justify-between p-7 h-3/4 border-4 w-1/3 m-auto ">
+                    <div className="flex border-dashed rounded-3xl flex-col text-center justify-between p-7 h-3/4 border-4 w-1/3 m-auto ">
                         <div className="flex flex-col">
-                            <p className="text-xl">
-                                Welcome to
+                            <p className="text-xl ">
+                                <b>Seja Bem-Vindo</b>
                             </p>
                             <h1 className="fontTitle text-8xl text-[#0CC17A]">
                                 Shellcc
                             </h1>
-                            <p className="w-3/4 m-auto text-left">
-                            A Shellcc é a parceira perfeita para auxiliar na administração de ativos e no desenvolvimento do seu negócio. Com especialidade em acompanhar a desvalorização e maximizar os valores, disponibilizamos suporte estratégico e personalizado para promover o sucesso do seu negócio.
+                            <p className="w-3/4 m-auto text-center">
+                            <br/>A Shellcc auxilia na administração de ativos e no desenvolvimento do seu negócio. Com foco em maximizar valores, oferecemos suporte estratégico e personalizado para impulsionar seu sucesso.
                             </p>
                         </div>
                         <div>
                             <p className="text-lg">Ainda não possui conta?</p>
-                            <button className="text-2xl bg-[#FFB800] p-4 rounded-xl px-20 text-black ">Criar conta</button>
+                            <button className="text-2xl bg-[#FFB800] p-4 rounded-xl px-20 text-[#0e1125] font-sans ">Criar conta</button>
                         </div>
                     </div>
                     <div className="w-[30%] m-auto flex flex-col justify-center">
@@ -43,7 +43,7 @@ function Login() {
 
                                 </div>
                                 <p className="text-right mb-6 ">Esqueceu a senha?</p>
-                                <button className="bg-[#0CC17A] w-3/4 text-2xl py-2 fo rounded-xl m-auto">Entrar</button>
+                                <button className="bg-[#0CC17A] w-3/4 text-2xl py-2 fo rounded-xl m-auto  hover:bg-green-300 font-sans text-[#0e1125]">Entrar</button>
                             </form>
                             <div className="flex flex-col justify-center items-center mt-3 gap-2">
                                 <p>
